5fe7ef4b75a336762c24346efc9c63ad87be70a7
Yan Lobianchi <yan.cruz@zgsolucoes.com.br>
2019-12-07 11:53:45 -0300

    init

---
+++ src/main/java/br/com/zglosa/ZglosaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zglosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZglosaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZglosaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zglosa/ZglosaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zglosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZglosaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
